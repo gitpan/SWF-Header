2f3ba73a823111bcc491e6b4fa39872854146c97
William Ross <wross@cpan.org>
2004-06-04 12:24:28 +0000

    Import of WROSS/SWF-Header-0.02 from CPAN.
    gitpan-cpan-distribution: SWF-Header
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         WROSS/SWF-Header-0.02.tar.gz
    gitpan-cpan-author:       WROSS
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,31 @@
+use strict;
+use lib qw( ../lib );
+use Test::More;
+
+BEGIN {
+    plan (tests => 14);
+    use_ok('SWF::Header');
+}
+
+my $h = SWF::Header->new;
+ok( $h, 'header object constructed' );
+
+my $header = SWF::Header->read_file('./test/banner.swf');
+my $expected = {
+    'version' => 6,
+    'xmin' => 0,
+    'filelen' => 11558,
+    'ymax' => 1200,
+    'width' => 468,
+    'xmax' => 9360,
+    'rate' => 24,
+    'count' => 1,
+    'duration' => 0.0416666666666667,
+    'signature' => 'CWS',
+    'ymin' => 0,
+    'height' => 60,
+};
+
+for ( qw(signature version filelen count rate duration xmin xmax ymin ymax width height) ) {
+    is($header->{$_}, $expected->{$_}, "$_ = $header->{$_}");
+}
